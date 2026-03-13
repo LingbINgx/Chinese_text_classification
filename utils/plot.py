0daebb8bc3ead09388378dddfab5d227faeff7da
LingbINgx <146912904+LingbINgx@users.noreply.github.com>
2026-03-13 22:56:08 +0800

    字体

--- utils/plot.py
+++ utils/plot.py
@@ -3,7 +3,11 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']  # 黑体
+matplotlib.rcParams['font.sans-serif'] = [  'SimHei',
+                                            'Microsoft YaHei',
+                                            'Noto Sans CJK SC',
+                                            'WenQuanYi Zen Hei'
+                                        ]
 matplotlib.rcParams['axes.unicode_minus'] = False    # 解决负号显示问题
 
 
@@ -13,5 +17,12 @@ def plot_confusion_matrix(y_true, y_pred, labels, title='Confusion Matrix'):
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
     disp.plot(cmap="Blues", ax=ax, values_format='.3f', colorbar=False)
     plt.title('Confusion Matrix')
-    plt.show()
+    #plt.show()
     fig.savefig(f"../image/{title}.png", dpi=300, bbox_inches="tight")
+    
+
+if __name__ == "__main__":
+    y_true = [0, 0, 1, 1, 2, 2]
+    y_pred = [0, 0, 1, 2, 2, 2]
+    labels = ['一你好世界', '二再见', '三深度学习']
+    plot_confusion_matrix(y_true, y_pred, labels)
